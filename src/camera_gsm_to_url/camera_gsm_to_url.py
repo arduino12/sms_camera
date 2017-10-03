79802faa03ec52df098c9203897dcfc4f172a034
arduino12 <arad.rgb@gmail.com>
2017-10-03 12:41:15 +0000

    reorder code lines

--- src/camera_gsm_to_url/camera_gsm_to_url.py
+++ src/camera_gsm_to_url/camera_gsm_to_url.py
@@ -2,7 +2,6 @@ import os
 import time
 import logging
 import datetime
-
 from PIL import Image
 from picamera import PiCamera
 
@@ -15,67 +14,68 @@ class CameraGsmToUrl(app.App):
 
     def __init__(self):
         app.App.__init__(self, constants)
-        self.left, self.top = 0, 0
-        # get current screen size
-        self.width, self.height = map(int, os.popen(r'tvservice -s | grep -oP "\d\d+x\d\d+"', 'r').read().strip().split('x'))
-        # attach to raspberrypi camera
-        self.camera = PiCamera()
-        self.camera.framerate = constants.CAMERA_FPS
-        # logos overlays
-        self.logo1 = self._image_to_overlay(constants.LOGO0_PATH)
-        self.logo2 = self._image_to_overlay(constants.LOGO1_PATH)
-        self.logo3 = self._image_to_overlay(constants.LOGO2_PATH)
-        # get camera pictures scale
-        self.camera_scale = self.camera.resolution.width / self.camera.resolution.height
         # last pictures overlays
         self.pictures = []
-        # draw evrything
-        self.redraw()
-
-    def redraw(self):
+        # attach to raspberrypi camera
+        self.camera = PiCamera()
+        # configure camera
+        if hasattr(constants, 'CAMERA_FPS'):
+            self.camera.framerate = constants.CAMERA_FPS
+        if hasattr(constants, 'CAMERA_RESOLUTION'):
+            self.camera.resolution = constants.CAMERA_RESOLUTION
+        if not hasattr(constants, 'CAMERA_RESIZE'):
+            constants.CAMERA_RESIZE = None
+        # get current screen size
+        self.left, self.top = 0, 0
+        self.width, self.height = map(int, os.popen(r'tvservice -s | grep -oP "\d\d+x\d\d+"', 'r').read().strip().split('x'))
+        # calc draw stuff
+        camera_scale = self.camera.resolution.width / self.camera.resolution.height
         self.sep = int(min(self.width, self.height) * constants.SEP_SCALE)
         self.pictures_h = int((self.height - 3 * self.sep) * constants.PICTURES_HEIGHT_SCALE)
-        self.pictures_w = int(self.camera_scale * self.pictures_h)
+        self.pictures_w = int(camera_scale * self.pictures_h)
         self.pictures_c = int((self.width - self.sep) / (self.pictures_w + self.sep))
         self.pictures_l = int((self.width - self.pictures_c * (self.pictures_w + self.sep) - self.sep) / 2) + self.sep + self.left
         self.pictures_t = self.sep + self.top
-
-        self._logger.info('main window: %s', (self.left, self.top, self.width, self.height))
-        self._logger.info('camera resolution: %sx%s @ %s fps', self.camera.resolution.width, self.camera.resolution.height, self.camera.framerate)
-        self._logger.info('first picture window: %s', (self.pictures_l, self.pictures_t, self.pictures_w, self.pictures_h))
-        self._logger.info('pictures count: %s, sep: %s', self.pictures_c, self.sep)
-        
-        if not self.camera.closed:
-            if not self.camera.previewing:
-                self.camera.start_preview()
-                self.camera.preview.fullscreen=False
-                self.logo1.layer = self.camera.preview.layer + 1
-                self.logo2.layer = self.logo1.layer
-                self.logo3.layer = self.logo1.layer
-            camera_t = self.sep + self.pictures_h + self.sep 
-            camera_h = self.height - camera_t - self.sep
-            camera_w = int(self.camera_scale * camera_h)
-            camera_l = int((self.width - camera_w) / 2) + self.left
-            self.camera.preview.window = [camera_l, camera_t, camera_w, camera_h]
-            self.logo1.window = [camera_l, camera_t, self.logo1.width, self.logo1.height]
-            self.logo2.window = [camera_l + camera_w - self.logo2.width, camera_t, self.logo2.width, self.logo2.height]
-            self.logo3.window = [camera_l, camera_t + camera_h - self.logo3.height, self.logo3.width, self.logo3.height]
+        camera_t = self.sep + self.pictures_h + self.sep 
+        camera_h = self.height - camera_t - self.sep
+        camera_w = int(camera_scale * camera_h)
+        camera_l = int((self.width - camera_w) / 2) + self.left
+        self._logger.info('\nmain window: %s sep: %s\ncamera resolution: %sx%s @ %s fps\nfirst picture window: %s count: %s', 
+            (self.left, self.top, self.width, self.height), self.sep,
+            self.camera.resolution.width, self.camera.resolution.height, self.camera.framerate,
+            (self.pictures_l, self.pictures_t, self.pictures_w, self.pictures_h), self.pictures_c)
+        # draw camera preview
+        self.camera.start_preview()
+        self.camera.preview.fullscreen = False
+        camera_topper_layer = self.camera.preview.layer + 1
+        self.camera.preview.window = [camera_l, camera_t, camera_w, camera_h]
+        # draw logos overlays
+        self.logo1 = self._image_to_overlay(constants.LOGO0_PATH, layer=camera_topper_layer)
+        self.logo2 = self._image_to_overlay(constants.LOGO1_PATH, layer=camera_topper_layer)
+        self.logo3 = self._image_to_overlay(constants.LOGO2_PATH, layer=camera_topper_layer)
+        self.logo1.window = [camera_l, camera_t, self.logo1.width, self.logo1.height]
+        self.logo2.window = [camera_l + camera_w - self.logo2.width, camera_t, self.logo2.width, self.logo2.height]
+        self.logo3.window = [camera_l, camera_t + camera_h - self.logo3.height, self.logo3.width, self.logo3.height]
+        # draw pictures overlays
         self.draw_pictures()
 
     def draw_pictures(self):
+        # draw pictures overlays
         for i, p in enumerate(self.pictures):
             p.window = (self.pictures_l + i * (self.pictures_w + self.sep), self.pictures_t, self.pictures_w, self.pictures_h)
 
     def take_picture(self):
+        # capture picture and return its path
         self.camera.preview.alpha = 100
         time.sleep(0.7)
-        path = constants.IMAGES_PATH % (datetime.datetime.now().strftime(constants.DATETIME_FORMAT),)
-        self.camera.capture(path) # resize=(480, 360)
+        path = constants.IMAGES_PATH % (datetime.datetime.now().strftime(constants.IMAGES_DATETIME_FORMAT),)
+        self.camera.capture(path, resize=constants.CAMERA_RESIZE)
         self.camera.preview.alpha = 255
         self.add_picture(path)
         return path
 
     def add_picture(self, path):
+        # add the given picture to pictures list
         self.pictures.append(self._image_to_overlay(path, resize=(self.pictures_w, self.pictures_h), transparent=False))
         if len(self.pictures) > self.pictures_c:
             self.pictures[0].close()
@@ -92,7 +92,7 @@ class CameraGsmToUrl(app.App):
         if resize is not None:
             img = img.resize(resize)
         # create required size (32, 16) padding for the image 
-        pad = Image.new('RGBA' if transparent else 'RGB', [((x + y - 1) // y) * y for x, y in zip(img.size, (32, 16))])
+        pad = Image.new('RGBA' if transparent else 'RGB', [((n + m - 1) // m) * m for n, m in zip(img.size, (32, 16))])
         # paste the original image into the padding
         pad.paste(img)
         # crearw image overlay, and return it with its size
--- src/camera_gsm_to_url/constants.py
+++ src/camera_gsm_to_url/constants.py
@@ -6,11 +6,14 @@ from infra.run.common import *
 
 
 CAMERA_EFFECTS = ['none', 'negative', 'solarize', 'sketch', 'emboss', 'hatch', 'watercolor', 'colorswap', 'posterise', 'cartoon']
-CAMERA_FPS = 30
+# CAMERA_FPS = 30
+# CAMERA_RESIZE = (480, 360)
+# CAMERA_RESOLUTION = (1680, 1050)
 PICTURES_HEIGHT_SCALE = 0.2
 SEP_SCALE = 0.005
 
 IMAGES_PATH = '/tmp/%s.png'
+IMAGES_DATETIME_FORMAT = DATETIME_FORMAT.replace('-', '_').replace(':', '_').replace(' ', '_')
 
 LOGO0_PATH, LOGO1_PATH, LOGO2_PATH = [os.path.join(BASIC_PATH, 'sms_camera', 'res', 'logo%s.png' % (i,)) for i in range(3)]
 
